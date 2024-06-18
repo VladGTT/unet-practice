44c313d20384132596e15e2a994f2236d44f3e55
Vlad <vladbozhko003@gmail.com>
2024-06-18 12:44:17 +0300

    master: implemented mnist conv net

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,19 @@
+mod training;
+mod data;
 mod model;
-use burn::tensor::Tensor;
-use burn::backend::Wgpu;
-
  
-// Type alias for the backend to use.
-type Backend = Wgpu;
+use burn::optim::AdamConfig;
+use burn::backend::{Autodiff, Wgpu, wgpu::AutoGraphicsApi};
+use crate::model::ModelConfig;
 
 fn main() {
-    let device = Default::default();
-    // Creation of two tensors, the first with explicit values and the second one with ones, with the same shape as the first
-    let tensor_1 = Tensor::<Backend, 2>::from_data([[2., 3.], [4., 5.]], &device);
-    let tensor_2 = Tensor::<Backend, 2>::ones_like(&tensor_1);
+    type MyBackend = Wgpu<AutoGraphicsApi, f32, i32>;
+    type MyAutodiffBackend = Autodiff<MyBackend>;
 
-    // Print the element-wise addition (done with the WGPU backend) of the two tensors.
-    println!("{}", tensor_1 + tensor_2);
+    let device = burn::backend::wgpu::WgpuDevice::default();
+    crate::training::train::<MyAutodiffBackend>(
+        "/tmp/guide",
+        crate::training::TrainingConfig::new(ModelConfig::new(10, 512), AdamConfig::new()),
+        device,
+    );
 }
