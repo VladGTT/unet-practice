f8e636f73148273e75c2bc9855af40002660e89a
Vlad <vladbozhko003@gmail.com>
2024-06-18 19:47:17 +0300

    testing2: found layer dimentions

--- src/model.rs
+++ src/model.rs
@@ -61,35 +61,35 @@ impl ModelConfig {
             conve1: Conv2dConfig::new([1,64], [3, 3]).init(device),
             conve2: Conv2dConfig::new([64,64], [3, 3]).init(device),
             
-            conve3: Conv2dConfig::new([,128], [3, 3]).init(device),
+            conve3: Conv2dConfig::new([32,128], [3, 3]).init(device),
             conve4: Conv2dConfig::new([128,128], [3, 3]).init(device),
             
-            conve5: Conv2dConfig::new([,256], [3, 3]).init(device),
+            conve5: Conv2dConfig::new([64,256], [3, 3]).init(device),
             conve6: Conv2dConfig::new([256,256], [3, 3]).init(device),
             
-            conve7: Conv2dConfig::new([,512], [3, 3]).init(device),
+            conve7: Conv2dConfig::new([128,512], [3, 3]).init(device),
             conve8: Conv2dConfig::new([512,512], [3, 3]).init(device),
 
             //Bottleneck
-            convb1: Conv2dConfig::new([1,], [3, 3]).init(device),
-            convb2: Conv2dConfig::new([,], [3, 3]).init(device),
+            convb1: Conv2dConfig::new([256,1024], [3, 3]).init(device),
+            convb2: Conv2dConfig::new([1024,1024], [3, 3]).init(device),
 
             //Decoder  
-            convdt1: ConvTranspose2dConfig::new([,], [2, 2]).with_stride([2,2]).init(device),
-            convd1: Conv2dConfig::new([,512], [3, 3]).init(device),
+            convdt1: ConvTranspose2dConfig::new([1024,512], [2, 2]).with_stride([2,2]).init(device),
+            convd1: Conv2dConfig::new([1024,512], [3, 3]).init(device),
             convd2: Conv2dConfig::new([512,512], [3, 3]).init(device),
             
-            convdt2: ConvTranspose2dConfig::new([, ], [2, 2]).with_stride([2,2]).init(device),
-            convd3: Conv2dConfig::new([, ], [3, 3]).init(device),
-            convd4: Conv2dConfig::new([, ], [3, 3]).init(device),
+            convdt2: ConvTranspose2dConfig::new([512,256], [2, 2]).with_stride([2,2]).init(device),
+            convd3: Conv2dConfig::new([512,256], [3, 3]).init(device),
+            convd4: Conv2dConfig::new([256,256], [3, 3]).init(device),
             
-            convdt3: ConvTranspose2dConfig::new([, ], [2, 2]).with_stride([2,2]).init(device),
-            convd5: Conv2dConfig::new([,], [3, 3]).init(device),
-            convd6: Conv2dConfig::new([,], [3, 3]).init(device),
+            convdt3: ConvTranspose2dConfig::new([256,128], [2, 2]).with_stride([2,2]).init(device),
+            convd5: Conv2dConfig::new([256,128], [3, 3]).init(device),
+            convd6: Conv2dConfig::new([128,128], [3, 3]).init(device),
            
-            convdt4: ConvTranspose2dConfig::new([, ], [2, 2]).with_stride([2,2]).init(device),
-            convd7: Conv2dConfig::new([,], [3, 3]).init(device),
-            convd8: Conv2dConfig::new([,], [3, 3]).init(device),
+            convdt4: ConvTranspose2dConfig::new([128,64], [2, 2]).with_stride([2,2]).init(device),
+            convd7: Conv2dConfig::new([128,64], [3, 3]).init(device),
+            convd8: Conv2dConfig::new([64,64], [3, 3]).init(device),
   
             activation: Relu::new(),
             pool: MaxPool2dConfig::new([2,2]).init() 
